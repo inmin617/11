08663c454e0fad92a68bfbd01e9c72a8d83bece0
inmin617 <144991175+inmin617@users.noreply.github.com>
2023-11-16 18:27:50 +0900

    실습4

--- main.c
+++ main.c
@@ -4,18 +4,22 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]){
-	int i=10;
-	char c='a';
-	int *iptr;
-	iptr=&i;
-	char *cptr;
-	cptr=&c;
-	int *iptr2;
-	iptr2=iptr;
 	
-	printf("i: %p\n%p (size:%i)\n", iptr, &i, sizeof(iptr));
-	printf("c: %p\n%p (size:%i)\n", cptr, &i, sizeof(cptr));
-	printf("iptr2: %p, %i\n", iptr2, *iptr2);
+	char *pc;
+	int *pi;
+	double *pd;
+	
+	pc=(char*)10000;
+	pi=(int*)10000;
+	pd=(double*)10000;
+	
+	printf("증가 전: pc=%p, pi=%p, pd=%p\n", pc, pi, pd);
+	
+	pc++;
+	pi++;
+	pd++;
+	
+	printf("증가 후: pc=%p, pi=%p, pd=%p\n", pc, pi, pd);
 	
 	return 0;
 	
